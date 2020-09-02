21d8e8059de82b0bdadabfa1417b85e5e84de00a
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-09-02 00:02:47 -0300

    Ativos, metadados e CSS

--- pages/posts/First-Post.js
+++ pages/posts/First-Post.js
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/Layout";
 
 const FirstPost = () => (
-  <>
-    <h1>FirstPost</h1>
+  <Layout>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <h1 className="h-page">FirstPost</h1>
     <h2>
       <Link href="/">
         <a>Back to Home</a>
@@ -10,7 +15,13 @@ const FirstPost = () => (
       <br />
       <a href="/">Back to Home tag a</a>
     </h2>
-  </>
+
+    <style jsx>{`
+      .h-page {
+        color: red;
+      }
+    `}</style>
+  </Layout>
 );
 
 export default FirstPost;
